5c6632438c0ceb3081c45b9b8af37fca31e91cd0
Taylan YILDIZ <taylanyildz@outlook.com>
2021-08-06 15:30:12 +0300

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const socketio = require('socket.io');
 const PORT = process.env.PORT || 3050;
 
 const app = express();
+app.get('/',(req,res)=>{
+    res.status.json({name:'Taylan'});
+});
 const server = app.listen(PORT, () => console.log('Server running : ' + PORT));
 
 
